f0fb357dccd3e342f25d3da7244ba90cc8aca069
Ahmed Abdulsalam <salshehu@gmail.com>
2023-12-11 22:23:43 +0000

    Display list of issues

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,13 +1,58 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumnHeaderCell,
+  TableHeader,
+  TableRow,
+} from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "../../../prisma/client";
 
-const Issues = () => {
+const Issues = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
-    <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+    <div className="m-auto p-5 ">
+      <div className="flex flex-row justify-between mb-3">
+        <h2 className="  ">Issue List</h2>
+        <Button>
+          <Link href="/issues/new">Add New</Link>
+        </Button>
+      </div>
+      <div>
+        <Table.Root variant="surface">
+          <TableHeader>
+            <TableRow>
+              <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+              <TableColumnHeaderCell className="hidden md:table-cell">
+                Status
+              </TableColumnHeaderCell>
+              <TableColumnHeaderCell className="hidden md:table-cell">
+                Created
+              </TableColumnHeaderCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {issues.map((issue) => (
+              <TableRow key={issue.id}>
+                <TableCell>
+                  {issue.title}{" "}
+                  <div className="block md:hidden">{issue.status}</div>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.status}
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table.Root>
+      </div>
     </div>
   );
 };
