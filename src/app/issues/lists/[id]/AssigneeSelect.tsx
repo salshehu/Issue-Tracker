bc364c0cb4be37b1c94e4f6d31e3a291b159795f
Ahmed Abdulsalam <salshehu@gmail.com>
2023-12-21 00:47:29 +0000

    Install hot toast and chart pack

--- src/app/issues/lists/[id]/AssigneeSelect.tsx
+++ src/app/issues/lists/[id]/AssigneeSelect.tsx
@@ -1,22 +1,42 @@
 "use client";
+import { Skeleton, Spinner } from "@/_components";
 import { Devs } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Heading, Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
-  const [devs, setDevs] = useState<Devs[]>([]);
+  // const [devs, setDevs] = useState<Devs[]>([]);
 
-  useEffect(() => {
-    const getDevs = async () => {
-      const res = await fetch("/api/devs");
-      if (!res.ok) return null;
+  // useEffect(() => {
+  //   const getDevs = async () => {
+  //     const res = await fetch("/api/devs");
+  //     if (!res.ok) return null;
 
-      const devlist = await res.json();
-      return setDevs(devlist);
-    };
+  //     const devlist = await res.json();
+  //     return setDevs(devlist);
+  //   };
 
-    getDevs();
-  }, []);
+  //   getDevs();
+  // }, []);
+
+  const {
+    data: devs,
+    error,
+    isLoading,
+  } = useQuery<Devs[]>({
+    queryKey: ["assignees"],
+    queryFn: () =>
+      fetch("/api/devs")
+        .then((res) => res.json())
+        .catch((e) => console.log(e)),
+    retry: 3,
+    staleTime: 60 * 1000, //list of entries will be cached for 60s i.e. 1mins
+  });
+
+  if (isLoading) return <Skeleton />;
+
+  if (error) return <Heading as="h6">Unable to load devs</Heading>;
 
   return (
     <Select.Root>
@@ -24,7 +44,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {devs.map((dev) => (
+          {devs?.map((dev) => (
             <Select.Item key={dev.id} value={dev.id}>
               {dev.userName}
             </Select.Item>
